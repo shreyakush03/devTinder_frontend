efe56dd3bf77571415fcb5c046676393d5169561
Shreya Kushwaha <shreyakush013@gmail.com>
2025-05-02 19:17:21 +0530

    routing

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile"
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path= "/login" element={<Login />} />
+            <Route path= "/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter> 
     </>
   )
 }
