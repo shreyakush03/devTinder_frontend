4506221b9f57b851eb5de8088c9284bf32c8b513
Shreya Kushwaha <shreyakush013@gmail.com>
2025-05-13 14:07:41 +0530

    login

--- my-vue-app/src/Login.jsx
+++ my-vue-app/src/Login.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
-  const [emailId, setEmailid] = useState("")
-  const [Password, setPassword] = useState("")
+  const [emailId, setEmailid] = useState("shreya@gmail.com")
+  const [password, setPassword] = useState("Shreya@321")
 
+  // function that will actually make an api call to login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:7777/login", {
+        emailId,
+        password
+      })
+    }
+    catch {
+      console.error(err)
+    }
+  }
 
   return (
     <div className="flex items-center justify-center my-10">
@@ -23,12 +36,12 @@ const Login = () => {
             <input
               type="text"
               className="input px-5"
-              value={Password}
+              value={password}
               onChange={(e) => setPassword(e.target.value)} />
           </fieldset>
 
           <div className="card-actions justify-end my-3">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
           </div>
         </div>
       </div>
